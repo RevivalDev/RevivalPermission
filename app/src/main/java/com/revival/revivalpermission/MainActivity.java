4d142f7b6805b8863c4e301b60882184652bf93b
刘伟 <13716101847@163.com>
2022-09-06 18:30:16 +0800

    v1.0.0

--- app/src/main/java/com/revival/revivalpermission/MainActivity.java
+++ app/src/main/java/com/revival/revivalpermission/MainActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.revival.permission.LogUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LogUtils.showLog("测试成功");
     }
 }
--- permission/src/main/java/com/revival/permission/LogUtils.java
+++ permission/src/main/java/com/revival/permission/LogUtils.java
@@ -1,4 +1,18 @@
 package com.revival.permission;
 
+import android.util.Log;
+
 public class LogUtils {
+
+    private static final String TAG = "TAG";
+
+    public static void showLog(String msg){
+        Log.e(TAG,msg);
+    }
 }
+
+
+
+
+
+
